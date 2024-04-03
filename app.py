96948cd692cff923ee12e3f95dc90ab4f0440b28
Anomali99 <fatiqnur@gmail.com>
2024-04-03 23:28:28 +0700

    merapikan config

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from api import api as api_blueprint
-from config import connect
+from config import Config, connect
 
 
 app = Flask(__name__)
 app.register_blueprint(api_blueprint)
-db = connect(app)
+app.config.from_object(Config)
+connect(app)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5127, debug=False)
--- config.py
+++ config.py
@@ -2,16 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-def connect(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5127@localhost/bukuku'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['UPLOAD_FOLDER'] = "static/upload"
+def connect( app):
     db.init_app(app)
 
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Methods', 'POST')
-        return response
-    return db
+class Config:
+    SQLALCHEMY_DATABASE_URI= 'postgresql://postgres:5127@localhost/bukuku'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER= "static/upload"
